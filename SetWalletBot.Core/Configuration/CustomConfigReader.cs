8a231c18642165d5ebf8453dddaf08ba6dacd70e
Дмитрий Ромашов <framebassman@gmail.com>
2017-09-09 15:11:28 +0300

    Add CustomConfigReader

---
+++ SetWalletBot.Core/Configuration/CustomConfigReader.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Noobot.Core.Configuration;
+
+namespace SetWalletBot.Core.Configuration
+{
+    public class CustomConfigReader : IConfigReader
+    {
+        private IConfigurationSection _configurationSection;
+        private const string SLACKAPI_CONFIGVALUE = "slack:apiTocken";
+        
+        public CustomConfigReader(IConfigurationSection configSection)
+        {
+            _configurationSection = configSection;
+        }
+        
+        public T GetConfigEntry<T>(string entryName)
+        {
+            return JObject.Parse(_configurationSection[entryName]).Value<T>();
+        }
+
+        public string SlackApiKey => GetConfigEntry<string>(SLACKAPI_CONFIGVALUE);
+        public bool HelpEnabled { get; set; } = true;
+        public bool StatsEnabled { get; set; } = true;
+        public bool AboutEnabled { get; set; } = true;
+    }
+}
--- SetWalletBot.Core/Startup.cs
+++ SetWalletBot.Core/Startup.cs
@@ -1,22 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Noobot.Core.Configuration;
+using SetWalletBot.Core.Configuration;
 
 namespace SetWalletBot.Core
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration { get; set; }
+        
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adds services required for using options.
+            services.AddOptions();
+
+            // Register the IConfiguration instance which MyOptions binds against.
+            //services.Configure<MyOptions>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,9 +40,9 @@ namespace SetWalletBot.Core
                 app.UseDeveloperExceptionPage();
             }
 
-            var configPath = Path.Combine(Directory.GetCurrentDirectory());
             
-            var noobHost = new NoobotHost(new ConfigReader(configPath));
+            
+            var noobHost = new NoobotHost(new CustomConfigReader(Configuration.GetSection("Bot")));
             applicationLifetime.ApplicationStarted.Register(noobHost.Start);
             applicationLifetime.ApplicationStopping.Register(noobHost.Stop);
         }
